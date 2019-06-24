f23bd1885ee2a34d511c87499814db3b6da0a4e6
costa1h <david12cst@gmail.com>
2019-06-24 20:36:09 -0300

    criando tarefas 1

---
+++ tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="task-grid">
+        <!--<div v-for="task in tasks" :key="task.name">
+            {{task.name}}
+        </div>-->
+        <Task v-for="task in tasks" :key="task.name" :task="task"></task>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            tasks: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
